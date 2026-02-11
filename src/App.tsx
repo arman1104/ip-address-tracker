e4132c7af83dd4bf76d273748d61463750b304f8
arman1104 <arman051215@gmail.com>
2026-02-11 16:24:57 +0530

    style: removed unwanted files

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-const App = () => {
+function App() {
   return (
-    <div>
+    <>
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
-    </div>
+    </>
   );
-};
+}
 
 export default App;
